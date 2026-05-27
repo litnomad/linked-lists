98ebf77d87d506a5e5a5c73e9a52a6d602c7069d
litnomad <128262375+litnomad@users.noreply.github.com>
2026-05-27 19:36:41 -0400

    prepend(), size(), firstNode(), at(), pop(), contains(), and findIndex()

--- script.js
+++ script.js
@@ -19,85 +19,150 @@ class LinkedList {
 
     if (this.head !== null) {
       // dummy variable for moving to the next node
-      let copy;
+      let current;
       if (!nextNode) {
-        copy = this.head;
+        current = this.head;
       } else {
-        copy = nextNode;
+        current = nextNode;
       }
       // recursion traverses through nodes in sequential order starting from head until null is reached
-      if (copy.nextNode !== null) {
-        nextNode = copy.nextNode;
+      if (current.nextNode !== null) {
+        nextNode = current.nextNode;
         return this.append(value, nextNode);
       }
       // creates a new node when null is reached
-      if (copy.nextNode == null) {
-        return (copy.nextNode = new Node(value));
+      if (current.nextNode == null) {
+        return (current.nextNode = new Node(value));
       }
     }
   }
 
-  prepend() {
-    // adds a new node containing value to the start of the list
+  // adds a new node containing value to the start of the list
+  prepend(value) {
+    const append = this.head;
+    this.head = new Node(value);
+    this.head.nextNode = append;
+    console.log("prepend", this.head);
+    return this.head;
   }
 
+  // returns the total nodes in the list
   size() {
-    // returns the total nodes in the list
+    let current = this.head;
+    let counter = 0;
+
+    while (current !== null) {
+      counter++;
+      current = current.nextNode;
+    }
+
+    return counter;
   }
 
+  // returns the value of the first node
   firstNode() {
-    // this returns the value of the first node
+    return this.head;
   }
 
-  at() {
-    // this returns value of the node at given index
+  // returns value of the node at given index
+  at(index) {
+    let current = this.head;
+    let i = 0;
+
+    while (i <= index && current !== null) {
+      if (i == index) {
+        break;
+      }
+      current = current.nextNode;
+      i++;
+    }
+
+    return current !== null ? current.value : null;
   }
 
+  // removes the head node and return its value; undefined if empty
   pop() {
-    // this removes the head node
+    if (this.head == null) {
+      return undefined;
+    }
+
+    const pop = this.head;
+    this.head = this.head.nextNode;
+    return pop;
   }
 
-  contains() {
-    // returns true if the passed value is in the list and otherwise false
+  // returns true if the passed value is in the list and otherwise false
+  contains(value) {
+    let current = this.head;
+
+    while (current !== null) {
+      if (current.value == value) {
+        return true;
+      }
+
+      current = current.nextNode;
+    }
+    if (current == null) {
+      return false;
+    }
   }
 
-  findIndex() {
-    // returns the index of the node containing the given value. If an index can't be found, return -1. If more than one node has a matching value, it should return the index of the first node with the matching value.
+  // returns the index of the node containing the given value. If an index can't be found, return -1. If more than one node has a matching value, returns the index of the first node with the matching value.
+  findIndex(value) {
+    let current = this.head;
+    let index = 0;
+
+    while (current !== null) {
+      if (current.value == value) {
+        return index;
+      }
+      current = current.nextNode;
+      index++;
+    }
+    if (current == null) {
+      return -1;
+    }
   }
 
-  // represents objects as strings. the format should be: ( value ) -> ( value ) -> ( value ) -> null
+  // represents objects as strings; format should be: ( value ) -> ( value ) -> ( value ) -> null
   toString(string = "", nextNode) {
     if (this.head == null) {
       string += "null";
-      console.log(string);
-      return;
+      return string;
     }
 
-    let copy;
+    let current;
     if (!nextNode) {
-      copy = this.head;
+      current = this.head;
     } else {
-      copy = nextNode;
+      current = nextNode;
     }
 
-    if (copy.value !== null) {
-      string += `( ${copy.value} ) -> `;
+    if (current.value !== null) {
+      string += `( ${current.value} ) -> `;
     }
-    if (copy.nextNode == null) {
+    if (current.nextNode == null) {
       string += "null";
     } else {
-      nextNode = copy.nextNode;
+      nextNode = current.nextNode;
       return this.toString(string, nextNode);
     }
-    console.log(string);
+
     return string;
   }
 }
 
 let list = new LinkedList();
-list.toString();
-list.append("dog");
+console.log(list.append("dog"));
 console.log(list.append("cat"));
-console.log(list.append(4));
-console.log(list.append(6));
-list.toString();
+console.log(list.append("igloo"));
+console.log(list.append("igloo"));
+console.log(list.size());
+console.log(list.firstNode());
+console.log(list.at(2));
+console.log(list.at(4));
+console.log(list.pop());
+console.log(list.toString());
+console.log(list.contains("igloo"));
+console.log(list.contains("dog"));
+console.log(list.findIndex("igloo"));
